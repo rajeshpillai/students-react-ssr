488dbd0c2e936c388a8c7fe0cbcc54108fe73f3a
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-12-04 13:09:37 +0530

    Creating client side components

---
+++ src/client/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home is where Heart is!</h2>
+            <button onClick={() => console.log("hello!")}>Click me!</button>
+        </div>
+    );
+};
+export default Home;
